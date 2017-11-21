449bdf95bd0dafa5de7de34f9693369df02a8b5e
Samuel Chen <samuelchen@Samuels-MacBook-Air.local>
2017-11-20 22:47:16 -0500

    the jdbc class

---
+++ cse305web/src/JDBC.java
@@ -0,0 +1,25 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+public class JDBC {
+
+	static JDBC instance;
+	public static final String dbname = "jijli";
+	public static final String URL = "jdbc:mysql://mysql2.cs.stonybrook.edu:3306/jijli";
+	public static final String USER = "jijli";
+	public static final String PASSWORD = "109843894";
+	public static final String DRIVER = "com.mysql.jdbc.Driver";
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName(DRIVER);
+		Connection conn = DriverManager.getConnection(URL, USER,PASSWORD);
+		return conn; 
+	}
+	
+	
+	
+}
